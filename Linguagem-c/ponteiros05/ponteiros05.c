af16f92477b06afe697cb08649dc99043017ebb0
Lekito <alexlekito075@gmail.com>
2021-09-07 01:08:30 -0300

    feat: exercise 5 of pointers.

---
+++ Linguagem-c/ponteiros05/ponteiros05.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+int main(void) {
+  int x = 10;
+  double y = 20.50;
+  char z = 'a';
+
+  int *pX = &x;
+  double *pY = &y;
+  char *pZ = &z;
+
+  int *resultado;
+  resultado = -447412328;
+
+  printf("Valor x = %i \n", resultado);
+  
+
+  getchar();
+
+  return 0;
+}
