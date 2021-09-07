3dc1c6cb1f6f363e1ec96c7a5e6c3edd4627b56e
Lekito <alexlekito075@gmail.com>
2021-09-07 01:16:03 -0300

    feat: exercise 7 of pointers.

---
+++ Linguagem-c/aula64/ponteiros07.c
@@ -0,0 +1,28 @@
+// Ponteiros com estruturas.
+#include <stdio.h> 
+
+int main (void) { 
+  
+  struct horario{
+    int hora;
+    int minuto;
+    int segundo;
+  };
+
+  struct horario agora, *depois;
+  depois = &agora;
+
+   (*depois).hora = 20; //primeira forma de passar ponteiros para estruturas 
+   depois -> minuto = 35; // foram mais usual de passar ponteiros para estruturas. 
+   depois -> segundo = 49;
+
+   int somatorio = 100;
+
+   struct horario antes;
+
+  printf("%i:%i:%i", agora.hora, agora.minuto, agora.segundo);
+
+  getchar();
+
+  return 0;
+}
