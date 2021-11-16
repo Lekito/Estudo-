0a4c120086c7cfa3eae31bc95d0cdd1a96ee3d07
Lekito <alexlekito075@gmail.com>
2021-11-16 20:01:16 -0300

    feat: add comment

--- C/aula_30/aula_30.c
+++ C/aula_30/aula_30.c
@@ -9,10 +9,10 @@ int main(void) {
 }
 
 void teste(void){
-  int variavelLocalAutomatica = 2;
+  int variavelLocalAutomatica = 2; // valor criado a cada chamada de função
   variavelLocalAutomatica *= 2;
 
-  static int variavelLocalEstatica = 2;
+  static int variavelLocalEstatica = 2; //mantem o valor
   variavelLocalEstatica *= 2;
 
   printf("Variavel local estatica %i\n", variavelLocalEstatica);
