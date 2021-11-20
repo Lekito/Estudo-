4e7ac7617e9b54cfe44a1c5729358d35e19b482e
Lekito <alexlekito075@gmail.com>
2021-11-20 01:11:46 -0300

    feat: pass arguments to the function

---
+++ C/aula_38/aula_38.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void) {
+  
+  void funcaoPrint(int num, int vetor[]);
+  int x = 10;
+  int vetor[3] = {10};
+
+  funcaoPrint(x, vetor);// essa função o vetor ta passando o enderço de memoria. Já o x passa somente uma copia. 
+
+  printf("Variavel int na funcao principal: %i \n", x );
+  printf("Vetor na funcao principal: %i\n", vetor[0] );
+
+  return 0;
+}
+
+void funcaoPrint(int x, int vet[]){
+
+  x = x + 10;
+  vet [0] = 20;
+
+  printf("Variavel int na funcao principal: %i \n", x );
+  printf("Vetor na funcao principal: %i\n", vet[0] );
+}
